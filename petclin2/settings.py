7b2877208344751509fb6317b7b8c5570851c3db
Ferdinandy Chagas <ferdinandsc@yahoo.com.br>
2024-12-12 20:36:53 -0300

    TG-3 #done

--- petclin2/settings.py
+++ petclin2/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-0f_1wung48!7v_=4l0uo=#*e-=*@&eiaa#j=n$hkqg9c3&cprs'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
